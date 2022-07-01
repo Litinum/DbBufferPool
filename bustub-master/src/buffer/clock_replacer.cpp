7ae28796159fa260eabab6bc5380336fe1561da4
android172 <63978434+android172@users.noreply.github.com>
2022-07-01 12:48:25 +0200

    Implementiran clock replacer
    Implementacija bez provere konkurentnosti.

--- bustub-master/src/buffer/clock_replacer.cpp
+++ bustub-master/src/buffer/clock_replacer.cpp
@@ -14,16 +14,36 @@
 
 namespace bustub {
 
-ClockReplacer::ClockReplacer(size_t num_pages) {}
+ClockReplacer::ClockReplacer(size_t num_pages) {
+  resident = boost::dynamic_bitset(num_pages);
+  clock = boost::dynamic_bitset(num_pages);
+}
 
 ClockReplacer::~ClockReplacer() = default;
 
-auto ClockReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+auto ClockReplacer::Victim(frame_id_t *frame_id) -> bool {
+  if (resident.any() == false) return false;
+  while (true) {
+    if (resident[current_frame]) {
+      if (clock[current_frame])
+        clock[current_frame] = 0;
+      else {
+        *frame_id = current_frame;
+        current_frame = (current_frame + 1) % clock.size();
+        return true;
+      }
+    }
+    current_frame = (current_frame + 1) % clock.size();
+  }
+}
 
-void ClockReplacer::Pin(frame_id_t frame_id) {}
+void ClockReplacer::Pin(frame_id_t frame_id) { resident[frame_id] = 0; }
 
-void ClockReplacer::Unpin(frame_id_t frame_id) {}
+void ClockReplacer::Unpin(frame_id_t frame_id) {
+  resident[frame_id] = 1;
+  clock[frame_id] = 1;
+}
 
-auto ClockReplacer::Size() -> size_t { return 0; }
+auto ClockReplacer::Size() -> size_t { return resident.count(); }
 
 }  // namespace bustub
